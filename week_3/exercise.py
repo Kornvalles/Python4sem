b6797ba2f56f8d78d49589b5b2dae1bcb5246587
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2020-03-09 22:05:10 +0100

    Pretty satisfied with this.

--- week_3/exercise.py
+++ week_3/exercise.py
@@ -1,6 +1,7 @@
 import random
 import string
 
+
 class Student():
 
     def __init__(self, name, gender, data_sheet, image_url):
@@ -14,9 +15,10 @@ class Student():
         for grade in grades:
             count += grade
         return count / len(grades)
-    
+
     def __str__(self):
-        return "name: {0}, gender: {1}, image_url: {2}".format(self.name, self.gender, self.image_url)
+        return "name: {0}, gender: {1}, image_url: {2}".format(
+            self.name, self.gender, self.image_url)
 
 
 class DataSheet():
@@ -29,12 +31,13 @@ class DataSheet():
         for course in self.courses:
             courses.append(course.grade)
         return courses
-    
+
     def __str__(self):
         return self.courses
 
+
 class Course():
-    
+
     def __init__(self, name, classroom, teacher, ETCS, grade=00):
         self.name = name
         self.classroom = classroom
@@ -42,34 +45,55 @@ class Course():
         self.ETCS = ETCS
         self.grade = grade
 
-course_name = ["Mathematics", "Chemistry", "English", "Science", "Psychology", "Geology", "History", "Art & Design"]
+
+course_name = ["Mathematics", "Chemistry", "English", "Science",
+               "Psychology", "Geology", "History", "Art & Design"]
 classrooms = [401, 402, 403, 404, 405, 406, 407, 408]
-teachers = ["Tom", "Susan", "Iris", "Isabel", "Jef", "Nick", "Martin", "Kathrin"]
-ETCSs = [20, 40, 60]
-grades = ["A", "B", "C", "D", "E", "Fx", "F"]
+teachers = ["Tom", "Susan", "Iris", "Isabel",
+            "Jef", "Nick", "Martin", "Kathrin"]
+grades = ["A", "A", "B", "B", "B", "B", "C", "C", "C", "C", "C", "C", "D", "D", "D", "D", "E", "E", "E", "Fx", "Fx", "F"]
 male_names = ["Alan", "Steve", "Roger", "Georg", "Dwayne", "Michael"]
 female_names = ["Michelle", "Brie", "Natascha", "Taylor", "Selena", "Sofia"]
 gender = ["Male", "Female"]
 
 
 def gen_students(n: int):
-    students: Student = list()
-    courses: Course = list()
     for i in range(n):
+        courses: Course = list()
         g = random.choice(gender)
         with open("./students.csv", "a") as f_obj:
             if g == "Male":
-                for j in range(0,4):
-                    courses.append(Course(random.choice(course_name), random.choice(classrooms), random.choice(teachers), random.choice(ETCSs), random.choice(grades)))
-                male = Student(random.choice(male_names), g, DataSheet(courses), "img"+str(i)+".jpg")
+                gen_courses(courses)
+                male = Student(random.choice(male_names), g,
+                                DataSheet(courses), "img" + str(i) + ".jpg")
                 for course in male.data_sheet.courses:
-                    f_obj.write("stud_name: {0}, course_name: {1}, teacher: {2}, ects: {3}, classroom: {4}, grade: {5}, img_url: {6}\n".format(male.name, course.name, course.teacher, course.ETCS, course.classroom, course.grade, male.image_url))
+                    f_obj.write(
+                        f"stud_name: {male.name}, course_name: {course.name}, teacher: {course.teacher}, ects: {course.ETCS}, classroom: {course.classroom}, grade: {course.grade}, img_url: {male.image_url}\n")
             else:
-                for j in range(0,4):
-                    courses.append(Course(random.choice(course_name), random.choice(classrooms), random.choice(teachers), random.choice(ETCSs), random.choice(grades)))
-                female = Student(random.choice(female_names), g, DataSheet(courses), "img"+str(i)+".jpg")
+                gen_courses(courses)
+                female = Student(
+                    random.choice(female_names),
+                    g,
+                    DataSheet(courses),
+                    "img" + str(i) + ".jpg")
                 for course in female.data_sheet.courses:
-                    f_obj.write("stud_name: {0}, course_name: {1}, teacher: {2}, ects: {3}, classroom: {4}, grade: {5}, img_url: {6}\n".format(female.name, course.name, course.teacher, course.ETCS, course.classroom, course.grade, female.image_url))
-    return students
+                    f_obj.write(
+                        f"stud_name: {female.name}, course_name: {course.name}, teacher: {course.teacher}, ects: {course.ETCS}, classroom: {course.classroom}, grade: {course.grade}, img_url: {female.image_url}\n")
+
+
+def gen_courses(courses):
+    new_course_names = course_name.copy()
+    new_classroms = classrooms.copy()
+    new_teachers = teachers.copy()
+    for _ in range(5):
+        courses.append(
+            Course(
+                new_course_names.pop(random.choice(range(len(new_course_names)))),
+                new_classroms.pop(random.choice(range(len(new_classroms)))),
+                new_teachers.pop(random.choice(range(len(new_teachers)))),
+                20,
+                random.choice(grades)))
+        
+
 
 gen_students(20)
